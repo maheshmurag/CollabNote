b999949a6f05aa3cf9b8fac731e2ee073bf3125f
Rolando Cruz <pspnando@me.com>
2015-07-18 20:59:59 -0700

    Removed transitions

--- CollabNote/www/js/app.js
+++ CollabNote/www/js/app.js
@@ -36,7 +36,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     });
 })
 
-.config(function ($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
     // Ionic uses AngularUI Router which uses the concept of states
     // Learn more here: https://github.com/angular-ui/ui-router
@@ -80,6 +80,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
         });
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/webHome');
+
+    $ionicConfigProvider.views.transition('none'); //Disables transitions
 
 })
 
